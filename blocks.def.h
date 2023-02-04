431f46869eb0bd019d184f63a32baf19148a80be
Sky Kosiner <yoni@kosiner.co.uk>
2023-02-04 16:22:57 +0000

    status bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "~/.local/bin/window-manger/spotify-control -status-wm",	1,		0},
+    {"", "~/.local/bin/statusbar/volume", 0, 10},
+    {"", "~/.local/bin/statusbar/batteryLife", 100, 0},
+    {"", "~/.local/bin/statusbar/weather", 1000, 0},
+    {"", "~/.local/bin/aircon-stuff/aircon status | sed s/{// | sed s/}//", 20, 5},
+    {"", "~/.local/bin/misc/lights status", 20, 3},
+	{"", "date +%I:%M%p",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
