be2583f773a12c0e1152dff659ba1549b6182dfe
Sky Kosiner <sky@skykosiner.co.uk>
2023-07-16 21:28:52 +0100

    Forget to commit this so fancy commit from me in the past

--- blocks.def.h
+++ blocks.def.h
@@ -7,7 +7,7 @@ static const Block blocks[] = {
     {"", "~/.local/bin/statusbar/weather", 1000, 0},
     {"🌤 ", "~/.local/bin/aircon-stuff/aircon status | sed s/{// | sed s/}//", 20, 5},
     {"💡 ", "~/.local/bin/misc/lights status", 20, 3},
-    {"✅ ", "org-today", 20, 0},
+    {"✅ ", "todoist-cli list --filter '(today | overdue)' | wc -l", 20, 0},
 	{"", "~/.local/bin/statusbar/timeDate",					60,		0},
 };
 
