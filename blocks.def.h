64f975fc5dbd1c9a91ac4e1129d44d198a9b3c2d
Sky Kosiner <sky@skykosiner.co.uk>
2023-11-20 14:49:03 +0000

    Forget to commit this so fancy commit from me in the past

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🎵 ", "~/.local/bin/window-manger/spotify-control -status-wm",	1,		0},
-    {"", "~/.local/bin/statusbar/volume", 0, 10},
-    {"", "~/.local/bin/statusbar/batteryLife", 100, 0},
-    {"", "~/.local/bin/statusbar/weather", 1000, 0},
-    {"🌤 ", "~/.local/bin/aircon-stuff/aircon status | sed s/{// | sed s/}//", 20, 5},
-    {"",	"network-stats",	1,	16},
-    // {"💡 ", "~/.local/bin/misc/lights status", 20, 3},
+	{"🎵 ", "spotify-control -status-wm",	1,		0},
+    {"", "volume", 0, 10},
+    {"", "batteryLife", 100, 0},
+    {"", "weather", 1000, 0},
+    {"🌤 ", "aircon status | sed s/{// | sed s/}//", 20, 5},
+    // {"",	"network-stats",	1,	16},
+    // {"💡 ", "misc/lights status", 20, 3},
     {"✅ ", "todoist-cli list --filter '(today | overdue)' | wc -l", 20, 0},
-    {"", "~/.local/bin/statusbar/mailbox", 180, 6},
-	{"", "~/.local/bin/statusbar/timeDate",					60,		0},
+    {"", "mailbox", 180, 6},
+	{"", "timeDate",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
