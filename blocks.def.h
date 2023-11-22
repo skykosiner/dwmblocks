29bac79756d2ea9e2bd9da451882c19e1ce0d4e8
Sky Kosiner <sky@skykosiner.co.uk>
2023-11-22 14:31:50 +0000

    Cganged some script stuff around

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🎵 ", "spotify-control -status-wm",	1,		0},
-    {"", "volume", 0, 10},
-    {"", "batteryLife", 100, 0},
-    {"", "weather", 1000, 0},
-    {"🌤 ", "aircon status | sed s/{// | sed s/}//", 20, 5},
+	{"🎵 ", "sb-music",	1,		0},
+    {"", "sb-volume", 0, 10},
+    {"", "sb-batteryLife", 100, 0},
+    {"", "sb-weather", 1000, 0},
+    {"🌤 ", "sb-aircon", 20, 5},
     // {"",	"network-stats",	1,	16},
     // {"💡 ", "misc/lights status", 20, 3},
-    {"✅ ", "todoist-cli list --filter '(today | overdue)' | wc -l", 20, 0},
-    {"", "mailbox", 180, 6},
-	{"", "timeDate",					60,		0},
+    {"✅ ", "sb-todoist", 20, 0},
+    {"", "sb-mailbox", 180, 6},
+	{"", "sb-timeDate",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
